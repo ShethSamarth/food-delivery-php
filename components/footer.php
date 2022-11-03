1e67412fa9b4aba3a928e355d7f4483e8dc4a9b3
ShethSamarth <samarth.sheth.29@gmail.com>
2022-11-03 18:07:54 +0530

    first commit

---
+++ components/footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:samarth.sheth.29@gmail.com" target="_blank">samarth.sheth.29@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>08:00am to 11:55pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="https://goo.gl/maps/zZRtQDn96c9kMcT28" target="_blank">Surat, Gujarat, India - 395007</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our contact</h3>
+         <a href="tel:9537685650" target="_blank">+91 9537685650</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span><a href="https://instagram.com/sheth_samarth" target="_blank" class="link">samarth sheth</a></span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
